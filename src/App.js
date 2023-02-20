e5605bd5cac8710f1db4f50ec166ccc9774ab5bd
Utkarsh Nigam <webxthedev@gmail.com>
2023-02-20 09:28:24 +0530

    first push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
+//import Header from './components/Header';
+// import Kissform from './pages/kissform';
+// import Kimsform from './pages/kimsform';
+import Kiitform from './components/kiitform';
+// import Successi from './pages/success';
+// import KiitData from './pages/kiitt';
+// import KimsData from './pages/kimst';
+// import NavBar from './pages/NavBar';
+// import Hospitalityform from './pages/hospitality';
+// import Templetrustform from './pages/templeform';
+//import Welcome from './components/Welcome';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+      {/* <Route exact path="/NavBar" element={<NavBar />} />
+        <Route path="/Kissform" element={<Kissform />} />
+        <Route path="/Kimsform" element={<Kimsform />} />
+        
+        <Route path="/KiitData" element={<KiitData />} /> */}
+        <Route path="/Kiitform" element={<Kiitform />} />
+      </Routes>
+    </Router>
+
+
+
+
+    // <React.Fragment>
+      
+    //   <main>
+    //     <Routes>
+    //     <Route path="/Kissform" element={<Kissform />} />
+    //     <Route path="/Kimsform" element={<Kimsform />} />
+    //     <Route path="/Kiitform" element={<Kiitform />} />
+    //     <Route path="/KiitData" element={<KiitData />} />
+    //     <Route path="/KimsData" element={<KimsData />} />
+    //     <Route path="/NavBar" element={<NavBar />} />
+    //     <Route path="/Hospitalityform" element={<Hospitalityform />} />
+    //     <Route path="/Templetrustform" element={<Templetrustform />} />
+        
+    //       <Route path="/Successful" element={<Successi />} />
+    //       {/* <Route path="/user" element={<Welcome/>} /> */}
+    //     </Routes>
+    //   </main></React.Fragment>
   );
 }
 
